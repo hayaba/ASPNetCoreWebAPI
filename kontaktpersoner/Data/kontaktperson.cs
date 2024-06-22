f9e3cb01b5393107a510236d6c3a3f0a8f0dda49
hayaba <hayabara995@gmail.com>
2024-06-22 13:12:04 +0200

    kontaktperson class proprities

--- kontaktpersoner/Data/kontaktperson.cs
+++ kontaktpersoner/Data/kontaktperson.cs
@@ -1,12 +1,27 @@
-﻿namespace kontaktpersoner.Data
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace kontaktpersoner.Data
 {
     internal sealed class kontaktperson
     {
+        [Key]
         public int KontaktId { get; set; }
-        public string Navn { get; set; }
-        public string Adresse { get; set; }
-        public string Email { get; set; }
-        public string Telefon { get; set; }
+
+        [Required(ErrorMessage = "Name is required")]
+        [StringLength(50, ErrorMessage = "Name can't be longer than 50 characters")]
+        public string Navn { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Address is required")]
+        [StringLength(200, ErrorMessage = "Address can't be longer than 200 characters")]
+        public string Adresse { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email address")]
+        public string Email { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Phone number is required")]
+        [Phone(ErrorMessage = "Invalid phone number")]
+        public string Telefon { get; set; } = string.Empty;
 
     }
 }

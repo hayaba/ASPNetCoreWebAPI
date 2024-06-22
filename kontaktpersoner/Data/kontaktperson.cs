2d20d79885b6da19c955cbecabc3a29146f874c8
hayaba <hayabara995@gmail.com>
2024-06-22 12:20:38 +0200

    data class

---
+++ kontaktpersoner/Data/kontaktperson.cs
@@ -0,0 +1,12 @@
+﻿namespace kontaktpersoner.Data
+{
+    internal sealed class kontaktperson
+    {
+        public int KontaktId { get; set; }
+        public string Navn { get; set; }
+        public string Adresse { get; set; }
+        public string Email { get; set; }
+        public string Telefon { get; set; }
+
+    }
+}

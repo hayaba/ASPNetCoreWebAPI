4fcea321a945b3e7df67e595e121e84c9eaea65f
hayaba <hayabara995@gmail.com>
2024-06-23 22:08:15 +0200

    buttons

--- reactclient/src/App.js
+++ reactclient/src/App.js
@@ -22,7 +22,13 @@ export default function App() {
     <div className="container">
       <div className="row min-vh-100">
         <div className="col d-flex flex-column justify-content-center align-items-center">
+          <div>
           <h1>ASP.NET Core Web API || Contacts List</h1>
+          <div className="mt-5">
+            <button onClick={getContacts} className="btn btn-dark btn-lg w-100">Get Contacts from server</button>
+            <button onClick={() => {}} className="btn btn-secondary btn-lg w-100 mt-4">Add New Contact</button>
+          </div>
+          </div>
 
           {renderContactsTable()}
         </div>
@@ -52,8 +58,8 @@ export default function App() {
               <td>email address</td>
               <td>12345678</td>
               <td>
-                <button className="btn btn-dark btn-lg mx-3 my-3">Edite</button>
-                <button className="btn btn-secondary btn-lg">delete</button>
+                <button className="btn btn-dark btn-s mx-3 my-3">Edit</button>
+                <button className="btn btn-secondary btn-s">Delete</button>
               </td>
             </tr>
           </tbody>

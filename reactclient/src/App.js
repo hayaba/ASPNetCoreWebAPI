cc2cc651124d00854fe1dbda1a077d405b6e5eae
hayaba <hayabara995@gmail.com>
2024-06-23 21:14:25 +0200

    bootstrap

--- reactclient/src/App.js
+++ reactclient/src/App.js
@@ -3,11 +3,11 @@ import React from "react";
 export default function App() {
   return (
     <div className="container">
-      <div className="row min-vh-100">
-        <div className="col d-flex flex-column justify-content-center align-items-center">
-          <h1>Hello</h1>
-        </div>
+    <div className="row min-vh-100">
+      <div className="col d-flex flex-column justify-content-center align-items-center">
+        <h1> Hello World</h1>
       </div>
     </div>
+  </div>
   );
 }

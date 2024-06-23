e3821d46240c2213f3a30f117cde583d36a05708
hayaba <hayabara995@gmail.com>
2024-06-23 21:00:32 +0200

    ..

--- reactclient/src/App.js
+++ reactclient/src/App.js
@@ -2,8 +2,12 @@ import React from "react";
 
 export default function App() {
   return (
-    <div>
-     Hello World
+    <div className="container">
+      <div className="row min-vh-100">
+        <div className="col d-flex flex-column justify-content-center align-items-center">
+          <h1>Hello</h1>
+        </div>
+      </div>
     </div>
   );
 }

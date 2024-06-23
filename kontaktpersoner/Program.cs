db501c4e2556ba3ae62e5107e24f1824d00083e2
hayaba <hayabara995@gmail.com>
2024-06-23 10:04:48 +0200

    lunch swagger

--- kontaktpersoner/Program.cs
+++ kontaktpersoner/Program.cs
@@ -1,22 +1,30 @@
+using kontaktpersoner.Data;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerUI;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(SwaggerGenOptionsExtensions =>
+{
+    SwaggerGenOptionsExtensions.SwaggerDoc("v1", new OpenApiInfo { Title = "kontaktpersoner list", Version = "v1" });
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI(SwaggerUIOptions =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    SwaggerUIOptions.DocumentTitle = "kontaktpersoner list";
+    SwaggerUIOptions.SwaggerEndpoint("/swagger/v1/swagger.json", "kontaktpersoner list v1");
+    SwaggerUIOptions.RoutePrefix = string.Empty;
+});
 
 app.UseHttpsRedirection();
 
+// map get request to get all kontaktPersoner from the database 
+app.MapGet("/get-all-kontaktPersoner", async () => await KontaktPersonerRepository.GetKontaktPersonerAsync());
 
 app.Run();
-
-

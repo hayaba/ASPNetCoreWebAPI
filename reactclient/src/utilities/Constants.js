0fc83a8e9fb5a35f27fb620e56b38b138b6d7367
hayaba <hayabara995@gmail.com>
2024-06-24 15:01:11 +0200

    ..

--- reactclient/src/utilities/Constants.js
+++ reactclient/src/utilities/Constants.js
@@ -1,5 +1,5 @@
 const API_BASE_URL_DEVELOPMENT = 'http://localhost:5299';
-const API_BASE_URL_PRODUCTION = 'https://appname.azurewebsites.net';
+const API_BASE_URL_PRODUCTION = 'https://kontaktpersoneraspnetcorereact.azurewebsites.net';
 
 const ENDPOINTS = {
     GET_ALL_CONTACTS: 'get-all-kontaktpersoner',
